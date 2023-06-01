cabe3aaf63900b88c48b9061d011c942e4cb87dc
vickmerys <carolina@gmail.com>
2023-05-31 20:46:47 -0500

    background

--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import NavBarPage from "../home/NavBar";
+import destination from "../destination/Destination";
+import imagenFondo from '../../assets/destination/background-destination-desktop.jpg';
+import imagen from "../../assets/destination/image-europa.png";
+
 
 const Destination = () => {
     return (
-     <>
-     <h1>Destination</h1>
+        < >
+     
+     <div className="divUno">
+        <tag>
+            
+        <img src={imagenFondo}/>
+        </tag>
+        <h1 className="divUno__title">
+          01 PICK YOUR DESTINATION  
+        </h1>
+        
+        <div> 
+        </div>
+     </div>
      </>
-
     )
 }
 
